993110bfa4afec6797007ca2cbff9556867ece56
Brendan Nofs <bnofs@brendans-mbp-2.enterprisenet.org>
2019-12-04 15:28:41 -0600

    Updates

---
+++ precache-manifest.57c67242d7bcf81ef7f9113549828341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313391f1392e40d238f445224bdcdac7",
+    "url": "/Leos/index.html"
+  },
+  {
+    "revision": "cffe1e65c6bc645094d4",
+    "url": "/Leos/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4021917b70fb11c40a36",
+    "url": "/Leos/static/js/2.99277f52.chunk.js"
+  },
+  {
+    "revision": "cffe1e65c6bc645094d4",
+    "url": "/Leos/static/js/main.b291b178.chunk.js"
+  },
+  {
+    "revision": "809be8294b50697f2524",
+    "url": "/Leos/static/js/runtime-main.2f511180.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Leos/static/media/logo.25bf045c.svg"
+  }
+]);
